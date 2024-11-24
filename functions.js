af5d71c746bd7708d06bc24366a1ef396d5daf00
ShanaxRules <shahnawazdeveloper21@gmail.com>
2024-11-25 05:17:06 +0600

    added functionalities

--- functions.js
+++ functions.js
@@ -1,34 +1,62 @@
+
+let total=0;
+
+
+
+
+
 function clicker(id){
-    document.getElementById(id).addEventListener('click' , function(){
+   
         const obj = document.getElementById(id);
-        if (!obj.className.includes('bg-[#1dd100]') || !obj.className.includes('bg-[#1DD100]')){
-            obj.className.remove('bg-gray-200');
-            obj.className.remove('text-gray-500');
-            obj.className.add('bg-[#1dd100]');
-            obj.className.add('text-white');
+        const Ul = document.getElementById('list-holder');
+        if (!obj.classList.contains('lg:bg-[#1dd100]') && !obj.classList.contains('lg:bg-[#1DD100]')){
+            
+            
+            obj.classList.remove('lg:bg-[#f7f8f8]');
+            obj.classList.remove('md:bg-[#f7f8f8]');
+            obj.classList.remove('bg-gray-200');
+            obj.classList.remove('text-gray-500');
+            obj.classList.add('lg:bg-[#1dd100]');
+            obj.classList.add('md:bg-[#1dd100]');
+            obj.classList.add('bg-[#1dd100]');
+            obj.classList.add('text-white');
+            const li = document.createElement('li');
+            li.id=obj.innerText;
+            li.className = "flex justify-between items-center py-2" ;
+            li.innerHTML=` 
+                            <div>
+                                <h1 class="text-gray-600">${obj.innerText}</h1>
+                            </div>
+                            <div>
+                                <h1 class="text-gray-600">Economy</h1>
+                            </div>
+                            <div>
+                                <h1 class="text-gray-600">550</h1>
+                            </div>
+                            `;
+            Ul.appendChild(li);
+
+            
+
         }
 
-        else{
-            obj.className.remove('bg-[#1dd100]');
-            obj.className.remove('text-white');
-            obj.className.add('bg-gray-200');
-            obj.className.add('text-gray-500');
+        else if (obj.classList.contains('lg:bg-[#1dd100]')){
+
+            let uler = document.getElementById('list-holder');
+            obj.classList.remove('lg:bg-[#1dd100]');
+            obj.classList.remove('md:bg-[#1dd100]');
+            obj.classList.remove('bg-[#1dd100]');
+            obj.classList.remove('text-white');
+            obj.classList.add('lg:bg-[#f7f8f8]');
+            obj.classList.add('md:bg-[#f7f8f8]');
+            obj.classList.add('bg-gray-200');
+            obj.classList.add('text-gray-500');
+            const objList = document.getElementsByClassName('lister');
+            const obj3 = document.getElementById(obj.innerText);
+            uler.removeChild(obj3);
+
         }
 
-    });
-}
-
-document.addEventListener('click' , function(event){
-    const objects1 = document.getElementsByClassName('holder-1');
-    const objects2 = document.getElementsByClassName('holder-2');
-    for (const obj of objects1){
-        if (obj.contains(event.target)){
-            console.log(obj);
-        }
     }
-    for (const obj of objects2){
-        if (obj.contains(event.target)){
-            console.log(event.target.value);
-        }
-    }
-});
+
+
