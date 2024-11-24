4e07297c7d560627cc4baae4caef321456e43562
ShanaxRules <shahnawazdeveloper21@gmail.com>
2024-11-25 02:18:54 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors : {
+                'new-green' : '#1DD100',
+
+            },
+        },
+    },
+    plugins: [],
+}
